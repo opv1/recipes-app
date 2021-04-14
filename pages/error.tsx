5cbbcdfef5209c20fc1fe5322a93d41a4278c330
opv1 <pas7273@gmail.com>
2021-04-14 19:12:59 +0300

    more beauty

--- pages/error.tsx
+++ pages/error.tsx
@@ -4,7 +4,10 @@ import styles from 'styles/error.module.scss'
 const ErrorPage: React.FC = () => {
   return (
     <div className={styles.error}>
-      <h1>Oops! The service is not available at the moment</h1>
+      <h1>
+        Oops! The service is not available at the moment (most likely the
+        limited number of API requests ended)
+      </h1>
     </div>
   )
 }
